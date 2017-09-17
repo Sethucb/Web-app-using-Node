d06d4c0b141b68fd141e6312a5650d57ce5f5e0e
Sethu Madhavan <sethu969@gmail.com>
2017-09-16 23:07:37 -0400

    Added front end validations

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 var dbconnect = require('./connect.js');
+var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 
 var db;
 
@@ -22,14 +26,21 @@ dbconnect.init().then(function(data){
 });
 
 app.get('/',function(req,res){
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendfile(path.join(__dirname+'/index.html'));
 });
 
-app.post('/',function(req,res){	
-	var obj = req.body;
+app.post('/form',function(req,res){	
+	console.log('Request ++++');	
+	let obj = req.body;
 	db.collection("Ombudsman_Entries").insert(obj);
 	// res.writeHead(200, {'Content-Type':'text/plain'});
 	// res.send(req);
+});
+
+app.post('/report',function(req,res){
+	let obj = req.body;
+	console.log('Request rece==== '+ obj.start_year);
+	// console.log('ob is '+ obj.beginDate + ' end is '+obj.endDate);
 });
 
 app.listen(3000,function(){
--- public/my-index.js
+++ public/my-index.js
@@ -1,10 +1,10 @@
 $(function(){
 
-	$('.btn').click(function(){
+	$('.btn-primary').click(function(){
 		alert("You have submitted the form");
 		
 		let obj = {};
-		$('.form-control option:selected').each(function(ele,value){
+		$('td .form-control option:selected').each(function(ele,value){
 			let val = value.innerHTML;
 			if(val == 'Select'){
 				val = '';
@@ -12,9 +12,8 @@ $(function(){
  	 		let name = value.parentElement.name;
  	 		obj[name] = val;
 		});
-		let url = 'http://127.0.0.1:3000';
 		$.ajax({
-			url: url,
+			url: '/form',
 			type: "POST",
 			data: obj,
 			dataType: "application/json",
@@ -25,9 +24,87 @@ $(function(){
 				console.log('Error is ',err);
 			}
 		});
-		// window.location.reload();
-		$('.form-control option:selected').each(function(ele,val){
-			val.innerHTML = 'Select';
+		// Defaults to Select
+		$('select').val('select');
+		$('.error p').html('');
+	});
+
+
+	$('.btn-info').click(function(){
+
+		let dates = {};
+		$('.rep option:selected').each(function(index,val){
+			let value = val.innerHTML;
+			if(value === 'Select'){
+				if(!$('div.error:contains("fields")').length){
+					$('<p>Error : Please enter all the fields</p>').appendTo('.error');
+					$('.error').show();
+				}
+				return false;
+			}
+			if(index === 0){
+				dates['start_month'] = val.index;
+			}
+			else if(index === 1){
+				dates['start_year'] = value;
+			}
+			else if(index === 2){
+				dates['end_month'] = val.index;
+			}
+			else{
+				dates['end_year'] = value;
+			}
+			if(Object.keys(dates).length === 4){
+					// $('.error').hide();
+					$('.error p').html('');
+					datesPromise(dates).then(function(data){
+						// $('.error').hide();
+						$('.error p').html('');
+						// toDateString .getMonth()
+						console.log('GOI into ajax');
+						// console.log('date is '+dates.start_year);
+						$.ajax({
+							url: "/report",
+							type: "POST",
+							data: dates,
+							dataType: "application/json",
+							success: function(data){
+								console.log('Success recieved');
+							},
+							error: function(err){
+								console.log('Error is ',err);
+							}	
+						});
+					}).catch(function(err){
+						if(!$('div.error:contains("date")').length){
+							$('<p>Error : '+ err + '</p>').appendTo('.error');
+							$('.error').show();
+						}						
+					});
+			}
 		});
 	});
+
+	let datesPromise = function(dates){
+		let start = new Date(dates.start_year +','+ dates.start_month);
+		let end = new Date(dates.end_year + ','+ dates.end_month);
+		
+		return new Promise(function(resolve,reject){
+			if(start < end){
+				// let beginDate = dates.start_year +',' + dates.start_month;
+				// let endDate = dates.end_year + ',' + dates.end_month;
+				// console.log('bg is '+beginDate+' e is '+endDate);
+				console.log('Good');
+				resolve(/*{
+					beginDate:beginDate,
+					endDate: endDate
+				}*/);
+			}
+			else{
+				console.log('Badddd');
+				reject('Please enter end date bigger than start date');
+			}
+		});
+	}
+
 });
